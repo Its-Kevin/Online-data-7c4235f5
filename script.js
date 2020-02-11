29c3201cd3812d07af506847a3ebb69c25c72bcf
Kevin_Isaza <2089276@talnet.nl>
2020-02-11 14:07:15 +0100

    Uitwerking van de exercise

---
+++ script.js
@@ -0,0 +1,9 @@
+let a = {
+  name: "Zara",
+  Score: 36,
+  Datum: "21 januari 2016",
+
+  Vrienden: "Mika, Josef, Maria, Sumail, Lotte",
+  Gekwalificeerd: "true"
+};
+console.log(JSON.stringify(a, null));
